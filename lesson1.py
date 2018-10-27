b192e3795c8f4fa74b1e90591f2d24d70167f1b6
James Vaughan <jvaughan40@gatech.edu>
2018-10-27 16:25:14 -0500

    update code for lesson 1

--- lesson1.py
+++ lesson1.py
@@ -13,7 +13,7 @@ def q2(answer):
     if answer == "O(n^2)":
         return "Great job!"
     elif answer == "O(n)":
-        return "Think about how many times the for loop will be performed and try again."
+        return "Think about how many times the for loop will be performed and remember the rules for combining Big O."
     elif answer == "O(1)":
         return "O(1) is equivalent to constant time. This effectively means there is no difference regardless of the size of the input. Think about what is happening in the for loop and try again."
 
@@ -23,7 +23,7 @@ def q3(answer):
     if answer == "O(m + n)":
         return "Great job!"
     elif answer == "O(n^2)":
-        return "Think about how many times the for loop will be performed and try again."
+        return "Think about the how many times the for loops will be performed and remember the rules for combining Big O."
     elif answer == "O(1)":
         return "O(1) is equivalent to constant time. This effectively means there is no difference regardless of the size of the input. Think about what is happening in the for loop and try again."
 
